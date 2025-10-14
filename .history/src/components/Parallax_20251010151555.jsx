f0af6ad4c0a5ea158f7c037bb4fcf3d1d04a6f4f
Pamela <pgbriones@gmail.com>
2025-10-14 13:22:09 +0200

    Remove history files and add to gitignore

--- .history/src/components/Parallax_20251010151555.jsx
+++
@@ -1,9 +0,0 @@
-const Parallax = () => {
-  return (
-    <section className="parallax-section">
-      <div className="parallax-bg" style={{backgroundImage: "url('/assets/runes.jpg')"}}></div>
-    </section>
-  )
-}
-
-export default Parallax
